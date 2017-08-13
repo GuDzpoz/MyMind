240c41988b3a76ada170408fc35bd13e1158804a
GuDzpoz <GuDzpoz@gmail.com>
2017-08-13 23:07:47 +0800

    Simple save operation.

--- app/src/main/java/moe/gensokyoradio/liberty/mymind/MindTreeActivity.java
+++ app/src/main/java/moe/gensokyoradio/liberty/mymind/MindTreeActivity.java
@@ -1,12 +1,14 @@
 package moe.gensokyoradio.liberty.mymind;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,9 +17,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 import moe.gensokyoradio.liberty.mymind.tree.ClickableNode;
 import moe.gensokyoradio.liberty.mymind.tree.MyMindTree;
@@ -69,6 +73,20 @@ public class MindTreeActivity extends AppCompatActivity {
         return stringBuilder.toString();
     }
 
+    private void writeAll(String path, String string) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(openFileOutput(mapPath, MODE_PRIVATE)));
+            writer.write(string);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            this.finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            this.finish();
+        }
+    }
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
@@ -111,5 +129,28 @@ public class MindTreeActivity extends AppCompatActivity {
             default:
                 return super.onContextItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map_operations, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.saveOption) {
+            writeAll(mapPath, this.tree.toJSON());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
